7c3fb159595b565f06a727bc1b03a0d0510ff404
puneeth <anything888z@gmail.com>
2026-04-29 00:19:40 +0530

    remove old layout and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,20 @@ import Admin from "./pages/Admin";
 import MyBooking from "./pages/MyBooking";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup"
-import Layout from "./components/Layout";
 import "./Styles/app.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/book" element={<Layout><Book /></Layout>} />
-        <Route path="/admin" element={<Layout><Admin /></Layout>} />
-        <Route path="/my-booking" element={<Layout><MyBooking /></Layout>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/book" element={<Book />} />
+    <Route path="/admin" element={<Admin />} />
+    <Route path="/my-booking" element={<MyBooking />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+  </Routes>
+</BrowserRouter>
   );
 }
 
